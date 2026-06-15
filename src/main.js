555ea74b111ab32005028543949b15ea87ce6c68
Lucas Carrasco <lucasecarrasco1@gmail.com>
2026-06-15 19:56:53 -0300

    Configuracion docker

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log("UnaHur - Anti-Social net");
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.listen(PORT, async (err)=> {
+    if(err) {
+        console.error(err.message)
+        process.exit(1)
+    }
+    console.log(`App iniciada en el puerto ${PORT}`)
+})
