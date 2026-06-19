e45d6eb3f8e8cfddd8d65e699ca574e6c1059006
Cris Ramirez <ramirezcris.cpr@gmail.com>
2026-06-19 18:01:34 -0300

    feature : updateComment

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -14,6 +14,30 @@ const createComment = async (req, res) => {
   }
 };
 
+const updateComment = async (req, res) => {
+  try {
+    const {texto} = req.body;
+    const id = req.params.id;
+
+    if (!texto) {
+      res.status(400).json({message: "El texto no puede estar vacio"})
+      return
+    }
+    const comment = await Comment.findByIdAndUpdate(id, {texto}, { new: true, runValidators: true })
+
+    if (!comment) {
+      res.status(404).json({message: `Comentario con id ${id} no encontrado`})
+      return
+    }
+
+    res.status(200).json(comment)
+    return
+  } catch (err) {
+    res.status(500).json({ message : err.message })
+    return 
+  }
+}
+
 module.exports = {
-  createComment
+  createComment, updateComment
 };
--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { createComment } = require('../controllers/commentController');
+const { createComment, updateComment } = require('../controllers/commentController');
 
 const router = Router();
 
 router.post('/', createComment);
+router.put('/:id', updateComment)
 
 module.exports = router;
