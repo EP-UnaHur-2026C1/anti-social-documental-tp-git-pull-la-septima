43b1c2d9f8e96c88d71b6c311183738a7bb9f76a
NicolasDondero <nico.donderox@gmail.com>
2026-06-19 21:30:55 -0300

    feature: getTags y getTagByIg, routes incluidas

--- src/controllers/tagController.js
+++ src/controllers/tagController.js
@@ -4,8 +4,8 @@ const createTag = async (req, res) => {
     try {
         const tag = await Tag.create(req.body);
         res.status(201).json(tag);
-    } catch (err) {
-        res.status(500).json({ message:err.message });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 };
 
@@ -21,7 +21,31 @@ const deleteTag = async (req, res) => {
     }
 }
 
+const getTags = async (req, res) => {
+    try {
+        const tags = await Tag.find();
+        res.status(200).json(tags);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const getTagById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const tag = await Tag.findById(id)
+        if (!tag) {
+            res.status(404).json({message : `No se encontro un tag con id ${id}`})
+        }
+        res.status(200).json(tag);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
 module.exports = {
     createTag,
     deleteTag,
+    getTags,
+    getTagById
 }
--- src/routes/tagRoutes.js
+++ src/routes/tagRoutes.js
@@ -1,9 +1,11 @@
 const {Router} = require('express');
-const { createTag, deleteTag } = require('../controllers/tagController');
+const { createTag, deleteTag, getTags, getTagById } = require('../controllers/tagController');
 
 const router = Router();
 
 router.post('/', createTag);
 router.delete('/:id', deleteTag);
+router.get('/', getTags);
+router.get('/:id', getTagById);
 
 module.exports = router;
