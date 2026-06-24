3c45b21be4d86d03abed238f5266870ccaa4869c
Cris Ramirez <ramirezcris.cpr@gmail.com>
2026-06-24 16:44:35 -0300

    chore : agregado los middleware en tagRoutes

--- src/routes/tagRoutes.js
+++ src/routes/tagRoutes.js
@@ -1,12 +1,12 @@
 const {Router} = require('express');
 const { createTag, deleteTag, getTags, getTagById, deleteTag, updateTag } = require('../controllers/tagController');
+const { validateTagId, validateSchemaTag } = require('../middlewares/tag.middleware');
 
 const router = Router();
-
-router.post('/', createTag);
+router.post('/', validateSchemaTag, createTag);
 router.get('/', getTags);
-router.get('/:id', getTagById);
-router.delete('/:id', deleteTag);
-router.put('/:id', updateTag);
+router.get('/:id', validateTagId, getTagById);
+router.delete('/:id', validateTagId, deleteTag);
+router.put('/:id', validateSchemaTag, validateTagId, updateTag);
 
 module.exports = router;
