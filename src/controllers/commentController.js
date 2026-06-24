3ec14cd7ae8943142048260d8a401edd1f3d2494
NicolasDondero <nico.donderox@gmail.com>
2026-06-24 17:33:22 -0300

    fix: updateComment y createComment

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -2,41 +2,33 @@ const Comment = require('../models/Comment');
 
 const createComment = async (req, res) => {
   try {
-    const { texto, user, post } = req.body;
-    
-    if (!texto || !user || !post) {
-      return res.status(400).json({ message: 'texto, user y post son campos obligatorios.' });
-    }
-    const comment = await Comment.create({ texto, user, post });
-    res.status(201).json(comment);
+    const { texto } = req.body;
+    const { id_user, id_post } = req.params;
+    await Comment.create({
+            texto,
+            user: id_user,
+            post: id_post,
+            visible: true
+        });
+    res.status(201).json({ message: 'El comentario fue agregado correctamente' });
+
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(500).json({ message: `${err}` });
   }
 };
 
 const updateComment = async (req, res) => {
   try {
-    const {texto} = req.body;
-    const id = req.params.id;
-
-    if (!texto) {
-      res.status(400).json({message: "El texto no puede estar vacio"})
-      return
-    }
-    const comment = await Comment.findByIdAndUpdate(id, {texto}, { new: true, runValidators: true })
-
-    if (!comment) {
-      res.status(404).json({message: `Comentario con id ${id} no encontrado`})
-      return
-    }
-
-    res.status(200).json(comment)
-    return
+    const { texto } = req.body;
+    const comment = await Comment.findByIdAndUpdate(req.params.id, { texto }, { new: true });
+    res.status(200).json({ message: `El Comentario : ${comment.texto} actualizado` });
+    
   } catch (err) {
-    res.status(500).json({ message : err.message })
-    return 
+    res.status(500).json({ message: `${err}` });
   }
 }
+
+
 
 module.exports = {
   createComment, updateComment
