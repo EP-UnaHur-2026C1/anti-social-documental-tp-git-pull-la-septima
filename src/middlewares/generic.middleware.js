4bc236e88e1bc9d6c80bcf325b814f899b5d8825
Lucas Carrasco <lucasecarrasco1@gmail.com>
2026-06-24 16:10:49 -0300

    ft: validateSchema en genericMiddleware

--- src/middlewares/generic.middleware.js
+++ src/middlewares/generic.middleware.js
@@ -5,15 +5,34 @@ const validateObjectId = (Model) => {
         try {
             const { id } = req.params;
             if (!mongoose.isValidObjectId(id)) {
-                return res.status(400).json({ message: 'El id no es un ObjectId válido' });
+                res.status(400).json({ message: 'El id no es un ObjectId válido' });
+                return;
             }
             const instance = await Model.findById(id);
             if (!instance) {
-                return res.status(404).json({ message: `El id ${id} no fue encontrado` });
+                res.status(404).json({ message: `El id ${id} no fue encontrado` });
+                return;
             }
             next();
         } catch (err) {
             res.status(500).json({ message: `${err}` });
         }
     };
+};
+
+const validateSchema = (Schema) => {
+    return (req, res, next) => {
+        const { error } = Schema.validate(req.body, { abortEarly: false });
+        if (error) {
+            const errorMsj = error.details.map(e => e.message);
+            res.status(400).json(errorMsj);
+            return
+        }
+        next();
+    };
+};
+
+module.exports = {
+    validateObjectId,
+    validateSchema
 };
