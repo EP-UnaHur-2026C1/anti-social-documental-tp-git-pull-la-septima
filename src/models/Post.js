e90cf405eea92f1d18dd452e3ee3a2cadced01ff
Nicolas Blanco <nicolas.blanco@estudiantes.unahur.edu.ar>
2026-06-24 20:12:45 -0300

    refactor(model): agregado fecha de publicación y tags en Post.js

--- src/models/Post.js
+++ src/models/Post.js
@@ -3,14 +3,23 @@ const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema({
     texto : { 
         type : String, 
-        required : true 
+        required : true, 
+        maxLength : 255
     },
     user : {
         type : mongoose.Schema.Types.ObjectId,
         ref : 'User',
         required : true
+    },
+    tags: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Tag'
+    }],
+    fechaPublicacion: {
+        type: Date,
+        default: Date.now
     }
-}, { timestamps : true })
+}, { timestamps: true });
 
 const Post = mongoose.model('Post', postSchema)
 
