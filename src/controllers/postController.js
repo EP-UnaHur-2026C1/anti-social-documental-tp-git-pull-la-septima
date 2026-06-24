408b4bbc966e2c05d8d38b3edfe8443f0da28abf
Nicolas Blanco <nicolas.blanco@estudiantes.unahur.edu.ar>
2026-06-24 20:32:37 -0300

    feat: postController con Crud completo

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,16 +1,86 @@
-const Post = require("../models/Post")
+const Post = require('../models/Post');
 
 const createPost = async (req, res) => {
     try {
-        const post = await Post.create(req.body);
-        res.status(201).json(post);
+        const { texto, tags } = req.body;
+        const user = req.params.id;
+        const postCreado = await Post.create({
+            texto,
+            user,
+            tags: tags || [],
+            fechaPublicacion: new Date()
+        });
+        const resultado = await Post.findById(postCreado._id)
+            .populate('user')
+            .populate('tags');
+        res.status(201).json(resultado);
     } catch (err) {
-        res.status(400).json({ message:err.message });
+        res.status(500).json({ message: `${err}` });
     }
 };
 
+const deletePost = async (req, res) => {
+    try {
+        await Post.findByIdAndDelete(req.params.id_post);
+        res.status(200).json({ message: 'Post eliminado correctamente' });
+    } catch (err) {
+        res.status(500).json({ message: `${err}` });
+    }
+};
 
+const getPosts = async (_, res) => {
+    try {
+        const posts = await Post.find().populate('user').populate('tags');
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json({ message: `${err}` });
+    }
+};
 
-module.exports = { 
-    createPost 
-}
+const getAllPostsByUser = async (req, res) => {
+    try {
+        const posts = await Post.find({ user: req.params.id })
+            .populate('user')
+            .populate('tags');
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json({ message: `${err}` });
+    }
+};
+
+const getOnePostByUser = async (req, res) => {
+    try {
+        const post = await Post.findOne({
+            _id: req.params.id_post,
+            user: req.params.id
+        }).populate('user').populate('tags');
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json({ message: `${err}` });
+    }
+};
+
+const updatePostByUser = async (req, res) => {
+    try {
+        const { texto, tags } = req.body;
+        const update = { texto };
+        if (tags) update.tags = tags;
+        const post = await Post.findOneAndUpdate(
+            { _id: req.params.id_post, user: req.params.id },
+            update,
+            { new: true }
+        ).populate('user').populate('tags');
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json({ message: `${err}` });
+    }
+};
+
+module.exports = {
+    createPost,
+    deletePost,
+    getPosts,
+    getAllPostsByUser,
+    getOnePostByUser,
+    updatePostByUser
+};
