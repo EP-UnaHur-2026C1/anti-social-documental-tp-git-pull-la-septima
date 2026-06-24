cc67a78de676fd201ad7cb90e05d1a245632b335
Nicolas Blanco <nicolas.blanco@estudiantes.unahur.edu.ar>
2026-06-24 20:35:57 -0300

    feat:agregados deletePost, getPosts, getAllPostsByUser, getOnePostByUser, updatePostByUser

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,8 +1,27 @@
-const {Router} = require('express');
-const { createPost } = require('../controllers/postController');
+const { Router } = require('express');
+const {
+    createPost,
+    deletePost,
+    getPosts,
+    getAllPostsByUser,
+    getOnePostByUser,
+    updatePostByUser
+} = require('../controllers/postController');
+const {
+    validateSchemaPost,
+    validatePostId,
+    validatePostByUser,
+    validatePostTagsExist
+} = require('../middlewares/post.middleware');
+const { validateUserId } = require('../middlewares/user.middleware');
 
 const router = Router();
 
-router.post('/', createPost);
+router.post('/:id', validateSchemaPost, validateUserId, validatePostTagsExist, createPost);
+router.delete('/:id_post', validatePostId, deletePost);
+router.get('/', getPosts);
+router.get('/user/:id', validateUserId, getAllPostsByUser);
+router.get('/user/:id/post/:id_post', validateUserId, validatePostId, validatePostByUser, getOnePostByUser);
+router.put('/user/:id/post/:id_post', validateSchemaPost, validateUserId, validatePostId, validatePostByUser, validatePostTagsExist, updatePostByUser);
 
 module.exports = router;
