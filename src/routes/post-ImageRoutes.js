0b7b4e79f94969cdd781f8a0bf227b210061a193
Marcos <carrasco.marcososcar@gmail.com>
2026-06-24 21:08:51 -0300

    feat(post-image): completar rutas anidadas con middlewares de validación

--- src/routes/post-ImageRoutes.js
+++ src/routes/post-ImageRoutes.js
@@ -1,11 +1,27 @@
-const {Router} = require('express');
-const { createPostImage,getPostImageByPostId,getPostImages,deletePostImage,updatePostImage} = require('../controllers/post-ImageController');
+const { Router } = require('express');
+const {
+    createPostImage,
+    getPostImages,
+    getPostImageById,
+    updatePostImage,
+    deletePostImage
+} = require('../controllers/post-ImageController');
+const {
+    validatePostImageId,
+    validateSchemaPostImage,
+    validatePostImageByPost
+} = require('../middlewares/post-image.middleware');
+const { validateUserId } = require('../middlewares/user.middleware');
+const { validatePostId, validatePostByUser } = require('../middlewares/post.middleware');
 
 const router = Router();
 
-router.post('/', createPostImage);
-router.get('/post/:id_post', getPostImageByPostId);
-router.get('/', getPostImages);
-router.delete('/:id', deletePostImage);
-router.put('/:id', updatePostImage);
+const validateIdsAndPostByUser = [validateUserId, validatePostId, validatePostByUser];
+
+router.post('/user/:id/post/:id_post/images', validateSchemaPostImage, validateIdsAndPostByUser, createPostImage);
+router.get('/user/:id/post/:id_post/images', validateIdsAndPostByUser, getPostImages);
+router.get('/user/:id/post/:id_post/images/:id_pi', validateIdsAndPostByUser, validatePostImageId, validatePostImageByPost, getPostImageById);
+router.put('/user/:id/post/:id_post/images/:id_pi', validateSchemaPostImage, validateIdsAndPostByUser, validatePostImageId, validatePostImageByPost, updatePostImage);
+router.delete('/user/:id/post/:id_post/images/:id_pi', validateIdsAndPostByUser, validatePostImageId, validatePostImageByPost, deletePostImage);
+
 module.exports = router;
