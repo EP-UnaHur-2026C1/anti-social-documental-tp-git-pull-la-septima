c060166f94296f532c32f179aeb91a4e0d40beaa
Nicolas Blanco <nicolas.blanco@estudiantes.unahur.edu.ar>
2026-06-26 12:25:25 -0300

    feat: CORS agregado a main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 const connectDB = require('./config/db');
@@ -11,7 +11,8 @@ const postRoutes = require('./routes/postRoutes');
 const commentRoutes = require('./routes/commentRoutes');
 const tagRoutes = require('./routes/tagRoutes');
 const postImageRoutes = require('./routes/post-ImageRoutes');
-
+const corsOptions = { origin: process.env.ALLOWED_ORIGINS || 'http://localhost:3000',optionsSuccessStatus: 200 };
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
